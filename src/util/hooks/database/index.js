2495cc03443f6eb4559b486b0da82227eee8ec2e
Johannes Pichler <j.pichler@webpixels.at>
2019-02-12 01:02:13 +0100

    renamed folder form util to hooks

---
+++ src/util/hooks/database/index.js
@@ -0,0 +1,102 @@
+import Waterline, { Collection } from "waterline";
+import _ from "lodash";
+import WaterlineUtils from "waterline-utils";
+import sailsDiskAdapter from "sails-disk";
+import { generateModelsFromFiles } from "./helper/models/builder";
+
+/**
+ * getDatabase
+ * @param {*} opts
+ */
+export const getDatabase = async (opts = {}) => {
+  // console.log("getDatabase(opts), opts=", opts);
+  let datastores, adapters, defaultModelSettings;
+  if (typeof opts !== "object") {
+    throw new Error(
+      "Configuration Options for Waterline Database must be of type object {}"
+    );
+  }
+
+  // if (opts["datastores"]) {
+  datastores = _.defaultsDeep(
+    {
+      default: { adapter: "sails-disk" }
+    },
+    opts["datastores"] || {}
+  );
+  // }
+
+  // if (opts["adapters"]) {
+  adapters = _.defaultsDeep(
+    {
+      "sails-disk": sailsDiskAdapter
+    },
+    opts["adapters"] || {}
+  );
+  // }
+
+  if (opts["defaultModelSettings"]) {
+    defaultModelSettings = _.defaultsDeep(
+      opts["defaultModelSettings"],
+      {
+        migrate: false
+      },
+      defaultModelSettings
+    );
+  }
+
+  let config = {
+    adapters,
+    datastores,
+    models: await generateModelsFromFiles(app),
+    defaultModelSettings
+  };
+
+  // console.log("config: ", config);
+  const orm = await new Promise((resolve, reject) => {
+    // console.log(config);
+    // process.exit(0);
+    Waterline.start(config, (err, orm) => {
+      if (err) {
+        //   console.error(err);
+        reject(err);
+        return;
+      } else {
+        // console.log("orm:", orm);
+        // console.log("Waterline: ", Waterline);
+
+        WaterlineUtils.autoMigrations(app.config.models.migrate, orm, function(
+          err
+        ) {
+          if (err) {
+            // return done(err);
+            throw new Error(err);
+          }
+          resolve(orm);
+
+          // return done(undefined, orm);
+        });
+      }
+    });
+  });
+
+  app.models = orm.collections;
+  app.model = identity => {
+    identity = identity.replace("", "").toLowerCase();
+    return orm.collections[identity];
+  };
+  app.datastores = orm.datastores;
+  app.waterline = orm;
+  return {
+    orm: orm,
+    models: orm.collections,
+    datastores: orm.datastores,
+    model: identity => {
+      identity = identity.replace("", "").toLowerCase();
+      return Waterline.getModel(identity, orm);
+      return waterlineInstance.collections[identity];
+    }
+  };
+};
+
+export default getDatabase;
---
+++ src/util/hooks/files/filestructure.js
@@ -0,0 +1,97 @@
+import fs from "fs";
+import path from "path";
+
+import {
+  renderTemplateFile,
+  generateFolderAtPath,
+  generateFileAtPathWithContent,
+  readFileAtPath
+} from "./helpers";
+
+export default async rootPath => {
+  await generateFolderAtPath(path.join(rootPath, "/api"));
+  await generateFolderAtPath(path.join(rootPath, "/api/models"));
+  await generateFolderAtPath(path.join(rootPath, "/api/schema"));
+  await generateFolderAtPath(path.join(rootPath, "/api/policies"));
+  await generateFolderAtPath(path.join(rootPath, "/api/resolvers"));
+  await generateFolderAtPath(path.join(rootPath, "/config"));
+
+  const configFilesToCreate = [
+    {
+      fileName: "adapters.js",
+      template: "adapters.js.tpl"
+    },
+    {
+      fileName: "bootstrap.js",
+      template: "bootstrap.js.tpl"
+    },
+    {
+      fileName: "datastores.js",
+      template: "datastores.js.tpl"
+    },
+    {
+      fileName: "models.js",
+      template: "models.js.tpl"
+    },
+    {
+      fileName: "settings.js",
+      template: "settings.js.tpl"
+    },
+    {
+      fileName: "policies.js",
+      template: "config_policies.js.tpl"
+    }
+  ];
+
+  for (let item of configFilesToCreate) {
+    const data = await readFileAtPath(
+      path.join(__dirname, "../../../../templates/", item.template)
+    );
+    const result = await generateFileAtPathWithContent(
+      path.join(rootPath, "/config/", item.fileName),
+      data
+    );
+  }
+
+  // generate an initial resolver "api/resolvers/index.js"
+  const resolversData = await renderTemplateFile(
+    path.join(__dirname, "../../../../templates/resolvers_index.js.tpl")
+  );
+  await generateFileAtPathWithContent(
+    path.join(rootPath, "/api/resolvers/index.js"),
+    resolversData
+  );
+
+  // generate an initial resolver "api/resolvers/index.js"
+  const api_isAuthenticated = await renderTemplateFile(
+    path.join(__dirname, "../../../../templates/api_isAuthenticated.js.tpl")
+  );
+  await generateFileAtPathWithContent(
+    path.join(rootPath, "/api/policies/isAuthenticated.js"),
+    api_isAuthenticated
+  );
+
+  /**
+   * Generate a default api/schema/schema.graphql
+   */
+  const schemaPath = path.join(rootPath, "api/schema/schema.graphql");
+  if (!fs.existsSync(schemaPath)) {
+    fs.writeFileSync(
+      schemaPath,
+      `################################\n## AUTO GENERATED SCHEMA FILE ##\n################################\ntype Query {\n  hello: String!\n}`,
+      { flag: "wx" }
+    );
+  }
+
+  /**
+   * Generate a default api/schema/models.graphql
+   */
+  const modelSchemaPath = path.join(rootPath, "api/schema/models.graphql");
+  if (!fs.existsSync(modelSchemaPath)) {
+    fs.writeFileSync(
+      modelSchemaPath,
+      `################################\n## AUTO GENERATED SCHEMA FILE ##\n################################\n`,
+      { flag: "wx" }
+    );
+  }
+};
