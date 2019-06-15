1b12fd6d1e6faa5f503d7ceeec8a81273831f1f2
Johannes Pichler <j.pichler@webpixels.at>
2019-06-15 19:37:09 +0200

    fixed autom migrations

--- src/util/hooks/database/index.js
+++ src/util/hooks/database/index.js
@@ -70,19 +70,63 @@ export const getDatabase = async (opts = {}) => {
           dawnship.env !== "production" &&
           process.env.NODE_ENV !== "production"
         ) {
-          WaterlineUtils.autoMigrations(
-            dawnship.config.models.migrate,
-            orm,
-            function(err) {
-              if (err) {
-                // return done(err);
-                throw new Error(err);
-              }
-              resolve(orm);
+          try {
+            // Now that `waterline-schema` has validated that all of our associations are valid,
+            // loop through each model again to set the correct `columnType` for each singular association.
+            _.each(orm.collections, function eachModel(WLModel) {
+              // console.log(WLModel);
+              // process.exit(1);
+              // Loop through the normalized model and set a column type for each attribute
+              _.each(WLModel.attributes, function eachAttribute(
+                attrDef,
+                attrName
+              ) {
+                // If this attribute is a plural association, or not an association at all, skip it.
+                if (!attrDef.model) {
+                  return;
+                }
 
-              // return done(undefined, orm);
-            }
-          );
+                // Otherwise, this is a singular association.
+                //
+                // Find out the column type of the primary key attribute of the associated model
+                // so that we can use it as the column type of this singular association.
+                // (This is for the purpose of automigrations.)
+                var otherModelIdentity = attrDef.model;
+                var otherWLModel = config.models[otherModelIdentity];
+                var associatedPKAttrName = otherWLModel.primaryKey;
+                var associatedPKAttrDef =
+                  otherWLModel.attributes[associatedPKAttrName];
+                var derivedColumnType =
+                  associatedPKAttrDef.autoMigrations.columnType;
+                // Set the column type, mutating our WLModel inline.
+                // > Also set the column type within the `schema`
+                if (!attrDef.autoMigrations) {
+                  attrDef.autoMigrations = {};
+                }
+
+                attrDef.autoMigrations["columnType"] = derivedColumnType;
+                WLModel.schema[
+                  attrName
+                ].autoMigrations.columnType = derivedColumnType;
+              });
+            });
+
+            WaterlineUtils.autoMigrations(
+              dawnship.config.models.migrate,
+              orm,
+              function(err) {
+                if (err) {
+                  // return done(err);
+                  throw new Error(err);
+                }
+                resolve(orm);
+
+                // return done(undefined, orm);
+              }
+            );
+          } catch (e) {
+            console.error("Could not automigrate Database");
+          }
         } else {
           if (dawnship.config.models.migrate !== "safe") {
             dawnship.log(
