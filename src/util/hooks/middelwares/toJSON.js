966cde79d066005a8f189a9ceb666d76251b0087
Johannes Pichler <j.pichler@webpixels.at>
2019-06-19 15:22:50 +0200

    removed comment

--- src/util/hooks/middelwares/toJSON.js
+++ src/util/hooks/middelwares/toJSON.js
@@ -1,7 +1,4 @@
 export const toJsonMiddleware = async (resolve, root, args, context, info) => {
-  if (process.env.NODE_ENV !== "production") {
-    console.log("toJSON Middleware...");
-  }
   const result = await resolve(root, args, context, info);
   if (
     result &&
